037effb7a4a31c0242c1c25428361f843bcd6d12
mamckean12 <mamckean@gmail.com>
2023-09-25 13:24:50 -0400

    re-copy all files from local to fix deploy issue

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Hello CSCI E-39!/i);
+  const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
